c9aab1018a85792cbfd31c2ead2f1a6e8095e9a3
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-10-29 00:44:33 -0400

    commit tree b421bbd08e9afa81bc25299c84d2f80be126d4f9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each_with_index do |name,index|
+  puts "#{index + 1 }.#{name}."
+
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)# code an argument here
+ 
+  veggies.collect do |item| 
+    item.capitalize + "!"
+    
+  end
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+   calls.any? {|call| call.length > 4}
+
+
+   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(no_cheese)# code an argument here
+  cheese_types = ["cheddar", "gouda", "camembert"]      
+  cheese_types.find {|obj| obj.include?(gouda)}
+  
+    
+                 
+
+  
+
 end
